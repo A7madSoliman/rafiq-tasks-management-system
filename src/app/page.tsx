f978e53623bf6ab02af0cffdafa3f27def7d4abc
A7madSoliman <ahmadsoliman283@gmail.com>
2026-09-13 03:46:23 +0300

    chore: finalize project foundation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
-  return (
-    <main className="bg-background p-xl">
-      <div className="gap-md flex items-center">
-        <Button variant="secondary">Primary Action</Button>
-
-        <Button variant="primary">Secondary</Button>
-
-        <Button variant="ghost">Ghost Action</Button>
-      </div>
-    </main>
-  );
+  return <main>Taskly</main>;
 }
